8742e752814adfa69b11d0e5818526d3c185bdbd
akp705 <72743112+akp705@users.noreply.github.com>
2020-11-29 11:21:46 +0000

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,17 @@
+
+<script>
+$(function()
+{
+    function after_form_submitted(data)
+    {
+        if(data.result == 'success')
+        {
+            $('form#reused_form').hide();
+            $('#success_message').show();
+            $('#error_message').hide();
+        }
+        else
+        {
+            $('#error_message').append('<ul></ul>');
+        }
+</script>
